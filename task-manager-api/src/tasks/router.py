320cac73c0ea161c0156a2890d3aa8ba12033db8
l-Spook-l <uaspookua@gmail.com>
2026-03-18 00:39:17 +0200

    feat: implement tasks GET and POST endpoints

--- task-manager-api/src/tasks/router.py
+++ task-manager-api/src/tasks/router.py
@@ -1,5 +1,8 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, status, HTTPException
 
+from src.tasks.dependencies import get_task_service
+from src.tasks.schemas import TaskCreate, TaskUpdate, TaskResponse
+from src.tasks.service import TaskService
 
 router = APIRouter(
     prefix="/tasks",
@@ -7,14 +10,19 @@ router = APIRouter(
 )
 
 
-@router.post("/tasks")
-async def create_task(new_task):
-    pass
+@router.get("/tasks", response_model=list[TaskResponse])
+def get_task(
+        service: TaskService = Depends(get_task_service)
+):
+    return service.get_tasks()
 
 
-@router.get("/tasks")
-async def get_task():
-    pass
+@router.post("/tasks", response_model=TaskCreate)
+def create_task(
+        new_task: TaskCreate,
+        service: TaskService = Depends(get_task_service)
+):
+    return service.create_task(new_task)
 
 
 @router.put("/tasks/{task_id}")
