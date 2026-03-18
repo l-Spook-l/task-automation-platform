c88750708aa0d9c8e9a3145b41e6eb9cc9dbeb2e
l-Spook-l <uaspookua@gmail.com>
2026-03-18 19:18:43 +0200

    feat: connect users router to main FastAPI app

--- task-automation-worker/src/main.py
+++ task-automation-worker/src/main.py
@@ -1,9 +1,13 @@
 import uvicorn
 from fastapi import FastAPI
 
+from src.users.router import router as router_user
+
 app = FastAPI(
     title="Task automation worker",
 )
+
+app.include_router(router_user, prefix="/api")
 
 
 if __name__ == "__main__":
