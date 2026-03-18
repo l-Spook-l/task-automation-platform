3634678075321e22886d9e77b7d40a85e214bdea
l-Spook-l <uaspookua@gmail.com>
2026-03-18 19:12:09 +0200

    feat: initialize FastAPI task automation worker application

---
+++ task-automation-worker/src/main.py
@@ -0,0 +1,10 @@
+import uvicorn
+from fastapi import FastAPI
+
+app = FastAPI(
+    title="Task automation worker",
+)
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", port=8000, host="0.0.0.0", reload=True)
