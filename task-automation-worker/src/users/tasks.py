aab2bc27a9cb210fbb34857d4d34fccf8a1223b4
l-Spook-l <uaspookua@gmail.com>
2026-03-19 23:56:27 +0200

    fix: add missing self argument to bound Celery task

--- task-automation-worker/src/users/tasks.py
+++ task-automation-worker/src/users/tasks.py
@@ -7,7 +7,7 @@ logger = logging.getLogger(__name__)
 
 
 @celery_app.task(bind=True, autoretry_for=(Exception,), retry_backoff=True, retry_kwargs={"max_retries": 3})
-def fetch_and_save_users():
+def fetch_and_save_users(self):
     try:
         service = UsersService()
         service.fetch_and_save()
