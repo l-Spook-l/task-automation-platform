68cd686759024c0f7d716a726aa93fc722acab9d
l-Spook-l <uaspookua@gmail.com>
2026-03-19 13:56:35 +0200

    feat: initialize FastAPI app and register routes

---
+++ task-ml-service/src/main.py
@@ -0,0 +1,13 @@
+import uvicorn
+from fastapi import FastAPI
+
+from src.ml.router import router as router_ml
+
+app = FastAPI(
+    title="Prediction priority task",
+)
+
+app.include_router(router_ml, prefix="/api")
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", port=8000, host="0.0.0.0", reload=True)
